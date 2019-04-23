684e81abd00a316a32352d7bf189a7365b63b6e4
Joey Hunt <huntjos@uw.edu>
2019-04-22 17:11:10 -0700

    Revert "Update main.java"
    This reverts commit 06fc7d8bbacd6299aad77dd8b842dc8840e3dfab.

--- brainGoodBye/src/brainGoodBye/main.java
+++ brainGoodBye/src/brainGoodBye/main.java
@@ -3,8 +3,8 @@ package brainGoodBye;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubs
-			System.out.println("not printing");
+		// TODO Auto-generated method stub
+			System.out.println("printing");
 	}
 
 }
