82dd34e83df803f804b8e5c38ae71539da624fca
Hunter Lantz <lantzh@uw.edu>
2019-05-08 11:02:04 -0700

    text

--- brainGoodBye/src/brainGoodBye/AboutFrame.java
+++ brainGoodBye/src/brainGoodBye/AboutFrame.java
@@ -1,14 +1,19 @@
 package brainGoodBye;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 public class AboutFrame extends JFrame {
 
-	AboutFrame() {
+	public AboutFrame() {
 		this.setSize(400, 400);
+		JLabel lab = new JLabel();
+		lab.setText(Version.getVersion());
+		this.add(lab);
+		
+		this.pack();
 		this.setVisible(true);
-		this.add(new JTextArea(Version.getVersion()));
 	}
 
 }
