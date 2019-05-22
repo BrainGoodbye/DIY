fe9f08d045b6ff2bfcae536cdc189f5b42eeabfc
Joey Hunt <huntjos@uw.edu>
2019-05-22 10:46:05 -0700

    Added ProjectPanel and SearchPanel.

---
+++ brainGoodBye/src/GUI/ProjectPanel.java
@@ -0,0 +1,54 @@
+package GUI;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+
+/**
+ * @author Joey Hunt
+ *
+ * 
+ */
+public class ProjectPanel extends JPanel {
+
+	/**
+	 * A default serial UID.
+	 */
+	private static final long serialVersionUID = -5768616998602501013L;
+	
+	/**
+	 * 
+	 */
+	private JScrollPane scrollPane;
+	
+	/**
+	 * 
+	 */
+	private JPanel searchPanel;
+	
+	/**
+	 * 
+	 */
+	private JPanel thumbnailPanel;
+
+	/**
+	 * 
+	 */
+	ProjectPanel() {
+		scrollPane = new JScrollPane();
+		searchPanel = new SearchPanel();
+		thumbnailPanel = new JPanel();
+		
+		initialize();
+	}
+	
+	/**
+	 * 
+	 */
+	private void initialize() {
+		thumbnailPanel.add(scrollPane);
+		
+		add(searchPanel);
+		add(thumbnailPanel);
+	}
+}
