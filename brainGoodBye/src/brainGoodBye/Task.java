f87ff442d349f372b645857d551c6d5c8d693f85
Jacob Ficker <jacobwficker@gmail.com>
2019-06-04 15:02:42 -0700

    Added comments to Task class

--- brainGoodBye/src/brainGoodBye/Task.java
+++ brainGoodBye/src/brainGoodBye/Task.java
@@ -6,52 +6,96 @@ public class Task implements Cloneable {
 	private String myDescription;
 	private boolean isDone;
 	
+	/**
+	 * @author Jacob Ficker
+	 * @param hours
+	 * @param desc
+	 */
 	public Task(Double hours, String desc) {
 		myDescription = desc;
 		myHoursToComplete = hours;
 		isDone = false;
 	}
 	
+	/**
+	 * @author Jacob Ficker
+	 * @param hours
+	 * @param desc
+	 * @param done
+	 */
 	public Task(Double hours, String desc, boolean done) {
 		myDescription = desc;
 		myHoursToComplete = hours;
 		isDone = done;
 	}
 	
+	/**
+	 * @author Jacob Ficker
+	 * @return
+	 */
 	public Double getHoursToComplete() {
 		return myHoursToComplete;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 * @param myHoursToComplete
+	 */
 	public void setHoursToComplete(Double myHoursToComplete) {
 		this.myHoursToComplete = myHoursToComplete;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 * @return
+	 */
 	public String getDescription() {
 		return myDescription;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 * @param myDescription
+	 */
 	public void setDescription(String myDescription) {
 		this.myDescription = myDescription;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 * @return
+	 */
 	public boolean isDone() {
 		return isDone;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 * @param isDone
+	 */
 	public void setDone(boolean isDone) {
 		this.isDone = isDone;
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 */
 	public Task() {
 		myDescription = "A Task";
 		myHoursToComplete = 0.0;
 		isDone = false;
 	}
 	
+	/**
+	 * @author Jacob Ficker
+	 */
 	public Task clone() {
 		return new Task(myHoursToComplete, myDescription, isDone);
 	}
 
+	/**
+	 * @author Jacob Ficker
+	 */
 	@Override
 	public boolean equals(Object o) {
 		Task t = (Task) o;
@@ -60,6 +104,9 @@ public class Task implements Cloneable {
 				&& this.getHoursToComplete() == t.getHoursToComplete();
 	}
 	
+	/**
+	 * @author Jacob Ficker
+	 */
 	@Override
 	public int hashCode() {
 		return this.toString().hashCode();
